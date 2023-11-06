dedd42ebdbddb96729d188b2d4f392f471b02ba9
Aniket Zade <aniketzade4024@gmail.com>
2023-11-07 00:35:37 +0530

    done

---
+++ src/components/Certificate.jsx
@@ -0,0 +1,37 @@
+import styles from "../CSS/certificate.module.css"
+import bannerElement1 from "../media/CerElement.png"
+
+const Certificate =  ()=>{
+
+    
+    
+    return <div id={styles.certificate}>
+        <div>
+            <h1>CERTIFICATE</h1>
+            <h3>OF ACHIEVEMENT</h3>
+            <h4>PROUDLY PRESENT TO</h4>
+            <input type="text" />
+            <h5>LOREM IPSCUM DOLOR SIT AMET CONSRCTETUR</h5>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo eum repellendus iure quaerat suscipit nostrum nulla architecto quasi ex dicta iusto quisquam, laborum animi ut nemo aliquid perspiciatis aut quod doloribus minima dignissimos? Vitae tenetur, voluptas placeat, tempora, ipsum assumenda fugiat animi neque officia inventore tempore qui qui.</p>
+            <div>
+                <div>
+                    <div>
+
+                    </div>
+                    <p>DATE</p>
+                </div>
+                <img src={bannerElement1} alt="" />
+                <div>
+                    <div>
+
+                    </div>
+                    <p>SIGNATURE</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+}
+
+export default Certificate;
